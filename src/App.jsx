ac4f6a4b397d47905fecb1110b8e213341736490
Giovanny Jardim <gigiojardim11@gmail.com>
2024-03-27 23:49:06 -0300

    versao setimo finalizada mobile

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,23 @@ export default function App() {
       <section>
         <div className="bg-orange p-12 rounded-t-lg">
           <img src={iconSedans} alt="imagem do sedans" className="mb-7"/>
-          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
-          <p className="font-lexend mb-7">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-          <a className="font-lexend mb-7" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhite">Sedans</h2>
+          <p className="font-lexend mb-7 text-offwhite">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a className="font-lexend mb-7 bg-offwhite block px-10 py-3 w-fit rounded-3xl text-orange" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-green p-12">
           <img src={iconSuvs} alt="imagem do suvs" className="mb-7"/>
-          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Suvs</h2>
-          <p className="font-lexend mb-7">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-          <a className="font-lexend mb-7" href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhite">Suvs</h2>
+          <p className="font-lexend mb-7 text-offwhite">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a className="font-lexend mb-7 bg-offwhite block px-10 py-3 w-fit rounded-3xl text-green" href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-dark-green p-12 rounded-b-lg">
           <img src={iconLuxury} alt="imagem doS carros de luxo" className="mb-7"/>
-          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Suvs</h2>
-          <p className="font-lexend mb-7">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a className="font-lexend mb-7" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
+          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhite">Suvs</h2>
+          <p className="font-lexend mb-7 text-offwhite">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a className="font-lexend mb-7 bg-offwhite px-10 py-3 w-fit rounded-3xl text-dark-green" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
         </div>
       </section>
     </main>
